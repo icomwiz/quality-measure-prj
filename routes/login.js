d198f16a0432af987e1c6b5c623c70d121c2bd07
jaesungbong@gmail.com <shdkdl2!>
2016-10-18 14:03:35 +0900

    .

--- routes/login.js
+++ routes/login.js
@@ -1,16 +1,28 @@
+var login = require('../../../../Users/taehwan/Desktop/quality-measure-prj/models/login');
+
 var express = require('express');
 var router = express.Router();
+
 
 router.get('/', function(req, res, next) {
     res.render('login', { title: 'Icomwiz' });
 });
 
 router.post('/', function(req, res, next) {
-    console.log(req.body);
-    if(req.body.c_id === 'aa' && req.body.c_pw === 'aa') {
-        return res.redirect('/main');
-    }
+    var user = {};
+    user.email = req.body.c_id;
+    user.pw = req.body.c_pw;
 
-    next(new Error('Check ID or PW'));
+    login.login(user, function(err, result) {
+        if(err) {
+            return next(err);
+        }
+        console.log(result);
+        if(result) {
+            return res.redirect('/main');
+        }
+        res.redirect('/login');
+    });
 });
+
 module.exports = router;
