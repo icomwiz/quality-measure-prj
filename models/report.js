3c69353f4750779981767c0c5e535c0e2f8b4276
jaesungbong@gmail.com <shdkdl2!>
2016-10-31 17:15:07 +0900

    .

--- models/report.js
+++ models/report.js
@@ -186,7 +186,6 @@ function newReport(info, callback) {
     });
 }
 
-
 // INPUT: teamName, OUTPUT: teamName, date, location
 function getReportsByteamId(teamId, callback) {
     var sql_select_reports_order_by_date =
@@ -210,7 +209,220 @@ function getReportsByteamId(teamId, callback) {
     })
 }
 
+//리포트 업데이트 하기
+function updatePlan(plan, callback) {
+    var aesPassword = process.env.AES_PASSWORD; //AES패스워드
+
+    //부서 이름을 통해 부서가 존재하는지 찾음.
+    var sql_find_department_by_name = 'SELECT id departmentId ' +
+                                      'FROM department ' +
+                                      'WHERE name = ?';
+
+    //부서가 없다면 insert 하기
+    var sql_insert_department = 'INSERT INTO department(name) ' +
+                                'VALUES(?)';
+
+    //파트이름을 통해 파트가 존재하는지 찾기
+    var sql_find_part_by_name = 'SELECT id partId ' +
+        'FROM part ' +
+        'WHERE name = ?';
+
+    //파트가 없다면 insert하기
+    var sql_insert_part = 'INSERT INTO part(name) ' +
+                          'VALUES(?)';
+
+    //팀이름과 조이름을 통해 팀이 존재하는지 찾기. OUTPUT: teamId
+    var sql_find_team_by_team_name_and_team_no = 'SELECT id teamId ' +
+        'FROM team ' +
+        'WHERE name = ? AND team_no = ?';
+
+    //팀이 없다면 insert하기
+    var sql_insert_team = 'INSERT INTO team(name, team_no) ' +
+                          'VALUES(?, ?)';
+
+    //팀과 파트가 엮여 있는지 확인
+    var sql_select_teams_part = 'SELECT * FROM teams_parts ' +
+                                'WHERE team_id = ? AND part_id = ?';
+
+    //팀과 파트 엮기
+    var sql_insert_teams_part = 'INSERT INTO teams_parts(team_id, part_id) ' +
+                                'VALUES(?, ?)';
+
+    //email을 통해 employee가 존재하는지 찾음. OUTPUT: ID
+    var sql_find_employee_by_email = 'SELECT id ' +
+                                     'FROM employee ' +
+                                     'WHERE CAST(AES_DECRYPT(UNHEX(email), \'wiz\') AS CHAR) = ?';
+
+    //employee 가 없다면 insert하기
+    var sql_insert_employee =
+        'INSERT INTO employee(name, email, phone_number, password, team_id, team_position, department_id, department_position, equipment_name) ' +
+        'VALUES(?, ' +  //이름
+        'HEX(AES_ENCRYPT(?, ' + aesPassword + ')), ' + //이메일
+        'HEX(AES_ENCRYPT(?, ' + aesPassword+ ')), ' + //휴대폰번호
+        'HEX(AES_ENCRYPT(SHA2(\'1111\', 512), ' + aesPassword + ')), ' + //패스워드
+        '?, ' + //팀아이디
+        '?, ' + //팀 포지션
+        '?, ' + //부서아이디
+        '?, ' + //직책
+        '?)'; //장비
+
+    //employee가 있다면 update하기
+    var sql_update_employee = 'UPDATE employee ' +
+                              'SET name = ?, ' + //이름
+                                  'email = HEX(AES_ENCRYPT(?, ' + aesPassword + ')), ' + //이메일
+                                  'phone_number = HEX(AES_ENCRYPT(?, ' + aesPassword + ')), ' + //휴대폰번호
+                                  'team_id = ?, ' + //팀아이디
+                                  'team_position = ?, ' + //팀 포지션
+                                  'department_id = ?, ' + //부서아이디
+                                  'department_position = ?, ' + //직책
+                                  'equipment_name = ? ' + //장비이름
+                              'WHERE id = ?';
+
+    //report가 있는지 확인하기
+    var sql_select_report = 'SELECT id ' +
+                            'FROM report ' +
+                            'WHERE employee_id = ? AND team_id = ? AND date = STR_TO_DATE(?, \'%Y-%m-%d\')';
+
+    //report가 있다면 update하기
+    var sql_update_report =
+        'UPDATE report ' +
+        'SET team_name = ?, team_position = ?, team_member = (SELECT GROUP_CONCAT(name) teamMember ' +
+                                                             'FROM employee ' +
+                                                             'WHERE team_id = ?), equipment_name = ?, location = ?, calls = ?, car_number = ?, car_type = ?, car_manager = (SELECT name ' +
+                                                                                                                                                                           'FROM employee ' +
+                                                                                                                                                                           'WHERE team_id = ? AND team_position = ?), type = 0 ' +
+        'WHERE employee_id = ? AND team_id = ? AND date = STR_TO_DATE(?, \'%Y-%m-%d\')';
+
+    //report 가 없다면 insert하기
+    var sql_insert_report =
+        'INSERT INTO report(employee_id, team_id, team_name, team_position, team_member, equipment_name, date, location, calls, car_number, car_type, car_manager, type) ' +
+        'VALUES(?, ?, ?, ?, (SELECT GROUP_CONCAT(name) teamMember ' +
+                            'FROM employee ' +
+                            'WHERE team_id = ?), ?, STR_TO_DATE(?, \'%Y-%m-%d\'), ?, ?, ?, ?, (SELECT name ' +
+                                                                                              'FROM employee ' +
+                                                                                              'WHERE team_id = ? AND team_position = 3), 0)';
+
+    dbPool.getConnection(function(err, dbConn) {
+        if (err) {
+            return callback(err);
+        }
+
+        //part가 있는지 없는지 검사하고 있으면 냅두고 없으면 insert하는 함수
+        function setPart(callback) {
+            async.each(plan.partName, function(item, callback) {
+                findPartByName(item, function(err, partId) {
+                    if (err) {
+                        callback(err);
+                    }
+                    if (!partId) {
+                        insertPart(item, function(err, partId) {
+                           if (err) {
+                               return callback(err);
+                           }
+                           callback(partId);
+                        });
+                    } else {
+                        callback(partId);
+                    }
+                });
+            }, function(err) {
+               if (err) {
+                    callback(err);
+               } else {
+                    callback(null);
+               }
+            });
+        }
+
+        //team이 있는지 없는지 검사하고 있으면 냅두고 없으면 insert하는 함수
+        function setTeam(callback) {
+            findteamByNameAndNo(function (err, results) {
+                if (err) {
+                    return callback(err);
+                }
+                if (!results) {
+                    insertTeam(function(err, results) {
+                        if (err) {
+                            return callback(err);
+                        }
+                        callback(results);
+                    });
+                } else {
+                    callback(results);
+                }
+            })
+        }
+
+        //부서가 존재하는지 찾기
+        function findDepartmentByName(departmentName,callback) {
+            dbConn.query(sql_find_department_by_name, [departmentName], function(err, results) {
+                if (err) {
+                    return callback(err);
+                }
+                if (!results) {
+                    return callback(null, false);
+                }
+                callback(null, results[0]); //리턴 departmentId
+            });
+        }
+
+        //employee가 존재하는지 찾기.
+        function findEmployeeByEmail(callback) {
+            dbConn.query(sql_find_employee_by_email, [plan.email], function(err, results) {
+                if (err) {
+                    return callback(err);
+                }
+                callback(null, results[0]);
+            });
+        }
+
+        //part가 존재하는지 찾기.
+        function findPartByName(partName, callback) {
+            dbConn.query(sql_find_part_by_name, [partName], function(err, results) {
+                if (err) {
+                    return callback(err);
+                }
+                callback(null, results[0]);
+            });
+        }
+
+        //part insert하기
+        function insertPart(partName, callback) {
+            dbConn.query(sql_insert_part, [partName], function(err, results) {
+                if (err) {
+                    return callback(err);
+                }
+                var partId = results.insertId;
+                callback(null, partId);
+            })
+        }
+
+        //team이 존재하는지 찾기.
+        function findteamByNameAndNo(callback) {
+            dbConn.query(sql_find_team_by_team_name_and_team_no, [plan.teamName, plan.teamNo], function(err, results) {
+                if (err) {
+                    return callback(err);
+                }
+                callback(null, results[0]);
+            });
+        }
+
+        //team이 존재하지 않으면 insert하기
+        function insertTeam(callback) {
+            dbConn.query(sql_insert_team, [plan.teamName], function(err, results) {
+                if (err) {
+                    return callback(err);
+                }
+                var teamId = results.insertId;
+                callback(null, teamId);
+            });
+        }
+    });
+
+}
+
 module.exports.reportList = reportList;
 module.exports.addReport = addReport;
 module.exports.newReport = newReport;
 module.exports.getReportsByteamId = getReportsByteamId;
+module.exports.updatePlan = updatePlan;
--- routes/report.js
+++ routes/report.js
@@ -1,9 +1,11 @@
 var Report = require('../models/report');
+var Employee = require('../models/employee');
 var formidable = require('formidable');
 var path = require('path');
 var express = require('express');
 var date = require('date-utils');
 var fs = require('fs');
+var xlsx = require('xlsx');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
@@ -82,12 +84,69 @@ router.post('/planner', function(req, res, next) {
         var srcExcelPath = files.uploadExcelfile.path;
         var date = new Date();
         var dt = date.toFormat('YYYY-MM-DD HH24-MI-SS');
+        var today = date.toFormat('YYYY-MM-DD');
+        console.log(today);
 
         var excelFileName = req.user.name + ' ' + dt + path.extname(srcExcelPath);
         var destExcelPath = path.join(path.dirname(srcExcelPath), excelFileName);
         fs.rename(srcExcelPath, destExcelPath, function(err) {
             if (err) {
                 return next(err);
+            }
+            var plans = [];
+            var planner = xlsx.readFile(destExcelPath);
+            var worksheet1 = planner.Sheets['Sheet1'];
+            for (z in worksheet1) {
+                if (z.substring(1, z.length) > 2) {
+                    if (z.substring(0, 1) === 'A') { //날짜
+                        var plan = {};
+                        plan.date = worksheet1[z].v;
+                    } else if(z.substring(0,1) === 'B') { //부서
+                        plan.department = worksheet1[z].v;
+                    } else if(z.substring(0,1) === 'C') { //직책
+                        plan.departmentPosition = worksheet1[z].v;
+                    } else if(z.substring(0,1) === 'D') { //파트
+                        plan.partName = worksheet1[z].v.split(',');
+                    } else if(z.substring(0,1) === 'E') { //팀
+                        plan.teamName = worksheet1[z].v;
+                    } else if(z.substring(0,1) === 'F') { //조
+                        plan.teamNo = worksheet1[z].v;
+                    } else if(z.substring(0,1) === 'G') { //직위
+                        plan.teamPosition = worksheet1[z].v;
+                    } else if(z.substring(0,1) === 'H') { //이름
+                        plan.name = worksheet1[z].v;
+                    } else if(z.substring(0,1) === 'I') { //휴대폰 번호
+                        plan.phoneNumber = worksheet1[z].v;
+                    } else if(z.substring(0,1) === 'J') { //이메일
+                        plan.email = worksheet1[z].v;
+                    } else if(z.substring(0,1) === 'K') { //측정 장비
+                        plan.equipmentName = worksheet1[z].v;
+                    } else if(z.substring(0,1) === 'L') { //차량 번호
+                        plan.carNumber = worksheet1[z].v;
+                    } else if(z.substring(0,1) === 'M') { //차량 종류
+                        plan.carType = worksheet1[z].v;
+                    } else if(z.substring(0,1) === 'N') { //업무 후 차량 관리자
+                        plan.carManager = worksheet1[z].v;
+                    } else if(z.substring(0,1) === 'O') { //측정 지역
+                        plan.location = worksheet1[z].v;
+                    } else if(z.substring(0,1) === 'P') { //일일 측정 목표 호량
+                        plan.calls = worksheet1[z].v;
+                        plans.push(plan);
+                    }
+                }
+            }
+            console.log(plans);
+            for (var i = 0; i < plans.length; i++) {
+                if (plans[i].date === today) { //계획의 날짜가 오늘이면 바로 업데이트
+                    Report.updatePlan(plans[i], function(err, callback) {
+                        if (err) {
+                            return next(err);
+                        }
+
+                    });
+                } else { //계획의 날짜가 오늘이 아니면 cron을 통해 스케쥴링
+
+                }
             }
             res.send({
                 result: '계획서 업로드 완료!'
